b1f1f39b6a5c1926914cc93921d2995e3f6ba1e0
Alex <73722920+mentor-27@users.noreply.github.com>
2023-12-16 23:38:59 +0200

    Table creation develop (#3)

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,9 +12,17 @@ module.exports = {
     },
     extends: ['eslint:recommended', 'google'],
     rules: {
-        'semi': 1,
+        'semi': 0,
         'comma-dangle': 0,
-        "require-jsdoc" : 0,
-        "linebreak-style": 0
+        'require-jsdoc' : 0,
+        'linebreak-style': 0,
+        'arrow-parens': 0,
+        'quotes': 0,
+        'no-unused-vars': 0,
+        'object-curly-spacing': 0,
+        'eol-last': 0,
+        'operator-linebreak': 0,
+        'max-len': 0,
+        'indent': 0
     }
 }
